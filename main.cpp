2028c50c185d652ebfb64302018c1f889d5b0859
TermiNateVader <nathanbenstevens@gmail.com>
2021-05-08 09:55:47 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //adds header file iostream
+using namespace std; //this is so you don't have to put std:: in front of everything 
+int main() //declares the main function
+{
+  cout << "Hello World"; //prints Hello World to the console
+  return 0; //returns 0
+}
